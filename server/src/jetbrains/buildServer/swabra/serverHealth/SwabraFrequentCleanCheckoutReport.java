a9a4c77df0937d88b9d3bc80d52b357abc006783
Evgeniy.Koshkin <Evgeniy.Koshkin@jetbrains.com>
2013-04-17 10:06:17 +0000

    - introduced HealthStatus category - (minor) extracted HealthStatus severity from HealthStatusItem

--- server/src/jetbrains/buildServer/swabra/serverHealth/SwabraFrequentCleanCheckoutReport.java
+++ server/src/jetbrains/buildServer/swabra/serverHealth/SwabraFrequentCleanCheckoutReport.java
@@ -1,12 +1,10 @@
 package jetbrains.buildServer.swabra.serverHealth;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import jetbrains.buildServer.serverSide.SBuildType;
-import jetbrains.buildServer.serverSide.healthStatus.HealthStatusItem;
-import jetbrains.buildServer.serverSide.healthStatus.HealthStatusReport;
-import jetbrains.buildServer.serverSide.healthStatus.HealthStatusScope;
-import jetbrains.buildServer.serverSide.healthStatus.ResultConsumer;
+import jetbrains.buildServer.serverSide.healthStatus.*;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -19,6 +17,7 @@ import org.jetbrains.annotations.NotNull;
 public class SwabraFrequentCleanCheckoutReport extends HealthStatusReport {
   private static final String SWABRA_FREQUENT_CLEAN_CHECKOUT_TYPE = "swabraFrequentCleanCheckout";
   private static final String SWABRA_CLASHING_BUILD_TYPES = "swabraClashingBuildTypes";
+  private static final ItemCategory CATEGORY = new ItemCategory(SWABRA_FREQUENT_CLEAN_CHECKOUT_TYPE, "Same checkout directory but different Build files cleaner (Swabra) settings", ItemSeverity.INFO);
 
   @NotNull
   @Override
@@ -32,19 +31,22 @@ public class SwabraFrequentCleanCheckoutReport extends HealthStatusReport {
     return "Detect Build files cleaner (Swabra) settings which may cause frequent clean checkout";
   }
 
+  @NotNull
+  @Override
+  public Collection<ItemCategory> getCategories() {
+    return Collections.singleton(CATEGORY);
+  }
+
   @Override
   public void report(@NotNull final HealthStatusScope scope, @NotNull final ResultConsumer resultConsumer) {
-    if (!scope.isItemWithSeverityAccepted(HealthStatusItem.Severity.INFO)) return;
+    if (!scope.isItemWithSeverityAccepted(ItemSeverity.INFO)) return;
 
     final List<List<SBuildType>> result = new SwabraClashingConfigurationsDetector().getClashingConfigurations(scope.getBuildTypes());
 
     for (final List<SBuildType> group: result) {
       if(group.isEmpty()) continue;
 
-      final HealthStatusItem item = new HealthStatusItem(signature(group),
-                                                         "Same checkout directory but different Build files cleaner (Swabra) settings",
-                                                         HealthStatusItem.Severity.INFO,
-                                                         Collections.<String, Object>singletonMap(SWABRA_CLASHING_BUILD_TYPES, group));
+      final HealthStatusItem item = new HealthStatusItem(signature(group), CATEGORY, Collections.<String, Object>singletonMap(SWABRA_CLASHING_BUILD_TYPES, group));
 
       for(SBuildType affectedBuildType: group) {
         resultConsumer.consumeForBuildType(affectedBuildType, item);
