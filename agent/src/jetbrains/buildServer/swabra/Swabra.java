ac5166b80bdc98a2698fb269a6a63a3e79de3f7f
Victory.Bedrosova <victory.bedrosova@jetbrains.com>
2014-04-15 14:27:35 +0000

    more for TW-35645

--- agent/src/jetbrains/buildServer/swabra/Swabra.java
+++ agent/src/jetbrains/buildServer/swabra/Swabra.java
@@ -87,7 +87,7 @@ public final class Swabra extends AgentLifeCycleAdapter implements PositionAware
 
   @NotNull
   public PositionConstraint getConstraint() {
-    return PositionConstraint.first();
+    return PositionConstraint.UNDEFINED;
   }
 
   @Override
